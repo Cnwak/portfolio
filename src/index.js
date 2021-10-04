908ae83f8c9c2d43d206eb398fd2caa4f25d4c91
Chijindu <cnnwakama@gmail.com>
2021-10-04 21:35:39 +0100

    Hopeful fixing of slug

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ class App extends React.Component {
 
           <Route component={Portfolio} path='/projects' />
 
-          <Route component={SinglePost} path='/blog/:slug' />
+          <Route component={SinglePost} path='/:slug' />
 
           <Route component={Blog} path='/' exact />
 
